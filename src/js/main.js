a796ef74771ff22efd1bc5a48e510ec17ddaaa9a
Hristo Panev <hristopanev20@gmail.com>
2024-06-11 17:12:52 +0300

    test api

--- src/js/main.js
+++ src/js/main.js
@@ -40,7 +40,7 @@ async function getAllData() {
                 "Content-Type": "application/json",
             },
         });
-        const responseAsJson = await response.record.json();
+        const responseAsJson = await response.json();
         return responseAsJson;
     } catch (error) {
         console.log(error);
